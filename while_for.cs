fbe13749fc461adff1cd63b3a2b0b9162ec15659
woshiekao <48241992+woshiekao@users.noreply.github.com>
2020-03-23 23:03:08 +0800

    Add files via upload

---
+++ while_for.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int counter = 0;
+            //while (counter < 10)
+            //{
+            //    Console.WriteLine($"Hello World! The counter is : {counter}");
+            //    counter++;
+            //}
+            //int counter = 0;
+            //do
+            //{
+            //    Console.WriteLine($"Hello World! The counter is : {counter}");
+            //    counter++;
+            //} while (counter < 10);
+
+            for (int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine($"Hello World! The counter is : {counter}");
+            }
+        }
+    }
+}
